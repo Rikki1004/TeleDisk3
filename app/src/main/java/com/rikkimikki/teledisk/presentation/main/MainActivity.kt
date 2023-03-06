af7f3da9f05bd528041f3b0d17188cd788369e64
Daniil <rikki-mikki2@mail.ru>
2023-03-06 19:42:45 +0300

    add drawer

--- app/src/main/java/com/rikkimikki/teledisk/presentation/main/MainActivity.kt
+++ app/src/main/java/com/rikkimikki/teledisk/presentation/main/MainActivity.kt
@@ -9,26 +9,53 @@ import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.Window
+import android.widget.Toast
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.rikkimikki.teledisk.R
+import com.rikkimikki.teledisk.databinding.ActivityMainBinding
 import com.rikkimikki.teledisk.databinding.DialogInputTextBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        /*supportFragmentManager.beginTransaction()
-            .replace(R.id.main_view_container,MainFragment.newInstance())
-            .commit()*/
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+
+        //drawerLayout.openDrawer(R.id.drawer_layout)
+
+        /*appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )*/
+
+        navView.setNavigationItemSelectedListener {
+            Toast.makeText(this, "rsedc", Toast.LENGTH_SHORT).show()
+            return@setNavigationItemSelectedListener false
+        }
     }
+
+
     companion object {
         fun getInstance(context:Context): Intent {
             return Intent(context,MainActivity::class.java)
         }
     }
-
-
 }
